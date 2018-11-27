b3d93fab46d197b68789906938c169bddc84d11a
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-11-27 19:46:17 +0530

    added style to Navbar, Jumbotron and Matches component.

--- src/component/matches/Matches.js
+++ src/component/matches/Matches.js
@@ -4,60 +4,81 @@ import brigton from '../../images/team_icons/brighton.png';
 const Matches = (props) => {
   return(
     <div className="matches">
-      <div className="matches_header">Matches</div>
+      <div className="matches_container">
+      <div className="matches_header">
+<span>Matches</span></div>
+        <div className="all_matches">
       <div className="match">
         <div className="match_date">2018-11-26</div>
         <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
+          <div>
+            <img src={brigton} alt="Brigton Flag"/>
+            <div>Brigton</div>
+          </div>
+          <span className="score">1</span>
         </div>
         <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
+          <div>
+            <img src={brigton} alt="Brigton Flag"/>
+            <div>Brigton</div>
+          </div>
+          <span className="score">1</span>
         </div>
       </div>
       <div className="match">
         <div className="match_date">2018-11-26</div>
         <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
+          <div>
+            <img src={brigton} alt="Brigton Flag"/>
+            <div>Brigton</div>
+          </div>
+          <span className="score">1</span>
         </div>
         <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
+          <div>
+            <img src={brigton} alt="Brigton Flag"/>
+            <div>Brigton</div>
+          </div>
+          <span className="score">1</span>
         </div>
       </div>
-      <div className="match">
-        <div className="match_date">2018-11-26</div>
-        <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
+          <div className="match">
+            <div className="match_date">2018-11-26</div>
+            <div className="match_team">
+              <div>
+                <img src={brigton} alt="Brigton Flag"/>
+                <div>Brigton</div>
+              </div>
+              <span className="score">1</span>
+            </div>
+            <div className="match_team">
+              <div>
+                <img src={brigton} alt="Brigton Flag"/>
+                <div>Brigton</div>
+              </div>
+              <span className="score">1</span>
+            </div>
+          </div>
+          <div className="match">
+            <div className="match_date">2018-11-26</div>
+            <div className="match_team">
+              <div>
+                <img src={brigton} alt="Brigton Flag"/>
+                <div>Brigton</div>
+              </div>
+              <span className="score">1</span>
+            </div>
+            <div className="match_team">
+              <div>
+                <img src={brigton} alt="Brigton Flag"/>
+                <div>Brigton</div>
+              </div>
+              <span className="score">1</span>
+            </div>
+          </div>
         </div>
-        <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
-        </div>
+        <button className="matches_btn">See more matches</button>
       </div>
-      <div className="match">
-        <div className="match_date">2018-11-26</div>
-        <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
-        </div>
-        <div className="match_team">
-          <img src={brigton} alt="Brigton Flag"/>
-          <div>Brigton</div>
-          <div className="score">1</div>
-        </div>
-      </div>
-      <button className="btn">See more matches</button>
     </div>
   );
 };
--- src/component/navbar/Jumbotron.js
+++ src/component/navbar/Jumbotron.js
@@ -4,6 +4,11 @@ import featured_player from '../../images/featured_player.png';
 const Jumbotron = () => {
   return (
     <div className="jumbotron">
+      <div className="jumbotron_text">
+        <div>3</div>
+        <div><span>League</span></div>
+        <div>Championships</div>
+      </div>
       <div className="jumbotron_flag">
         <div/>
         <div/>
@@ -11,11 +16,6 @@ const Jumbotron = () => {
       </div>
       <div>
         <img src={featured_player} alt="Featured Player" className="jumbotron_image"/>
-      </div>
-      <div className="jumbotron_text">
-        <div>3</div>
-        <div>League</div>
-        <div>Championships</div>
       </div>
     </div>
   )
