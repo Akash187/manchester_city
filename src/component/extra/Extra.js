d9c0d7e1fa9a9992e63d80d1ecdfbfa3cf206110
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-11-28 02:14:33 +0530

    styling extra and footer component finished.

--- src/component/extra/Extra.js
+++ src/component/extra/Extra.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import jersey from '../../images/jersey.jpg';
+import Zoom from 'react-reveal/Zoom';
 
 const Extra = () => {
   return (
     <div className="extra">
       <div className="container">
-        <div>
+        <div className="extra_container">
           <div className="extra_header">
-            Win a Jersey
+            <Zoom left>
+              Win a Jersey
+            </Zoom>
           </div>
-          <img src={jersey} alt="jersey" className="jersey_img"/>
+          <Zoom right>
+            <img src={jersey} alt="jersey" className="jersey_img"/>
+          </Zoom>
         </div>
         <div className="extra_form">
           <div>Enter your email</div>
-          <input type="text" placeholder="Enter your email"/>
-          <button>Enroll</button>
-          <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda culpa cupiditate dolorem.
+          <div>
+            <input type="text" placeholder="Enter your email"/>
+          </div>
+          <button className="enroll_btn">Enroll</button>
+          <div className="extra_form_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda culpa cupiditate dolorem.
           </div>
         </div>
       </div>
